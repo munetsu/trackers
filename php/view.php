3e04337c6e52d1afe8cb309ab06cf76878c71682
yumefukuro <yumefukuro@gmail.com>
2019-01-24 15:52:25 +0900

    チューター詳細の置き換え

--- php/view.php
+++ php/view.php
@@ -5,9 +5,33 @@
 
         }
 
-        // サインアップ画面
-        public function signUp(){
-            
+        // チューター詳細表示画面
+        public function tuotorRender($tuotor,$age){
+            var_dump($tuotor,$age);
+            exit();
+            $view = '
+                <div>
+                    <div class="header">
+                        <p>'.$tuotor['familyNameCharacter'].$tuotor['firstNameCharacter'].'</p>
+                    </div>
+                    <div>
+                        <div><img src="'.$tuotor['img'].'" style="width:150px; height: 150px;"></div>
+                        <div></div>
+                    <div>
+                    <div>
+                        <table>
+                            <tr>
+                                <th>年齢：</th>
+                                <th>'.$age.'</th>
+                            </tr>
+                            <tr>
+                                <th>学歴：</th>
+                                <th></th>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            ';
         }
 
 
