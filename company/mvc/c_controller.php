207ab00518a7987c313f4fe1c77916bcf72726a0
yumefukuro <yumefukuro@gmail.com>
2019-03-14 10:28:15 +0900

    company_login部分

--- company/mvc/c_controller.php
+++ company/mvc/c_controller.php
@@ -8,6 +8,14 @@
             $this->c_judge();
         }
 
+        // password_sha256処理
+        private function password($password){
+            // password sha256
+            $password = h($password);
+            $password = hash_hmac('sha256' ,$password , False);
+            return $password;
+        }
+
         // 処理切り分け
         private function c_judge(){
 
@@ -15,8 +23,8 @@
             if($this->POST == 'signUp'){
 
                 // password sha256
-                $password = h($_POST['password']);
-                $password = hash_hmac('sha256' ,$password , False);
+                $password = $this->password($_POST['password']);
+
                 // データ配列化
                 $array = array();
                 $array['name'] = h($_POST['name']);
@@ -26,6 +34,31 @@
                 // modelへデータ引き継ぎ
                 $this->model = new C_MODEL;
                 $this->model->signUp($array);
+
+            }
+
+            // ログイン処理
+            if($this->POST == 'login'){
+
+                // password_sha256
+                $password = $this->password($_POST['password']);
+                // データ配列
+                $array = array();
+                $array['email'] = h($_POST['email']);
+                $array['password'] = $password;
+
+                // modelへ引き継ぎ
+                $this->model = new C_MODEL;
+                $res = $this->model->login($array);
+
+                // 条件分岐
+                if(!$res){
+                    echo 'ログインエラー';
+                    exit();
+                }else{
+                    echo 'ログインしました';
+                    exit();
+                }
 
             }
         }
