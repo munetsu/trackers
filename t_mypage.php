80be4959eb708df7cba2647993ba111032cda5b1
yumefukuro <yumefukuro@gmail.com>
2019-04-01 14:00:00 +0900

    t_howtoの書籍部分修正

--- t_mypage.php
+++ t_mypage.php
@@ -24,11 +24,12 @@
         // 審査通過者の初業務
         header('location: http://'.$_SERVER["HTTP_HOST"].'/trackers/t_howto.php?id='.$id);
         exit();
-    }else if($step == 2){
-    }else if($step != 99){
-        echo 'ログイン上の問題が発生しました';
-        eixt();
     }
+    // else if($step == 2){
+    // }else if($step != 99){
+    //     echo 'ログイン上の問題が発生しました';
+    //     exit();
+    // }
 
     // VIEW読み込み
     include('mvc/view.php');
