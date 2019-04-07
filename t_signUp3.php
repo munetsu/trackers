58a00bf8c6dfdc044d3ea367e4bb58287cb1c22a
yumefukuro <yumefukuro@gmail.com>
2019-04-07 21:22:28 +0900

    t_signUp3（cssスマホも）

--- t_signUp3.php
+++ t_signUp3.php
@@ -3,7 +3,7 @@
     include('funcs/funcs.php');
     chkSsid();
 
-    $id = $_GET['id'];
+    $id = $_SESSION['tuotor_id'];
     include('mvc/model.php');
     $model = new MODEL;
     $column = 'security_code';
@@ -22,22 +22,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>【Trackers】チューター登録</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/common.css">
+    <link rel="stylesheet" href="css/t_signUp3.css">
     <!-- jQuery本体-->
     <script src="http://code.jquery.com/jquery-3.2.1.min.js"></script>
 </head>
 <body>
     <div>
-        <p>ご利用されていたスマホアプリを登録してください(最大10個)</p>
-        <div>
-            <span>機種</span>
-            <select name="smartphone">
-                <option value="iPhone">iPhone</option>
-                <option value="Android">Android</option>
-            </select>
-            <span>アプリ名</span>
-            <input type="text" name="app">
-            <button id="btn">登録</button>
+        <p class="text">ご利用されていたスマホアプリを登録してください(最大10個)</p>
+        <div class="search">
+            <div>
+                <span>機種</span>
+                <select name="smartphone">
+                    <option value="iPhone">iPhone</option>
+                    <option value="Android">Android</option>
+                </select>
+            </div>
+            <div>
+                <span>アプリ名</span>
+                <input type="text" name="app">
+                <button id="btn">登録</button>
+            </div>
         </div>
         <div>
             <table class="appArea">
@@ -50,8 +57,8 @@
             </table>
         </div>
         <!-- 登録ボタン -->
-        <div>
-            <a href="" id="regBtn">登録</a>
+        <div class="register">
+            <a href="" id="regBtn">決定</a>
         </div>
     </div>
     <script>
