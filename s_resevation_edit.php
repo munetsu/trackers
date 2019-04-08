e71ac9c0d42ec27ac7e2ca499d7e101b852cbc3b
yumefukuro <yumefukuro@gmail.com>
2019-04-08 21:34:31 +0900

    s_resevation_edit（cssスマホも）

--- s_resevation_edit.php
+++ s_resevation_edit.php
@@ -50,6 +50,11 @@
     $offerFinishtimem[] = h($_POST['offerFinishtimem2']);
     $offerFinishtimem[] = h($_POST['offerFinishtimem3']);
     $offerFinishtimem = json($offerFinishtimem);
+
+    // view読み込み
+    include('mvc/view.php');
+    $view = new VIEW;
+    $views = $view->viewStudentTopbar($code['student_id'], $code['k_familyname'], $code['k_firstname']);
 ?>
 
 <!DOCTYPE html>
@@ -58,7 +63,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>【Trackers】</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/common.css">
+    <link rel="stylesheet" href="css/s_common.css">
+    <link rel="stylesheet" href="css/s_resevation_edit.css">
     <!-- jQuery本体-->
     <script src="http://code.jquery.com/jquery-3.2.1.min.js"></script>
     <!-- jQuery UI -->
@@ -69,8 +78,8 @@
     <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
 </head>
 <body>
-    <div>
-        <p>修正お願いします</p>
+    <div><?php echo $views ?></div>
+    <div class="main">
         <div>
             <p>面談希望日時</p>
             <form action="s_resevation_confirm.php" method="POST" name="booking">
@@ -80,30 +89,30 @@
                     第1希望：
                     <input type="text" name="offerDate1" class="datepicker" value=<?php echo $offerDate1 ?>>
                 </p>
-                <div>
-                    希望時間<br>
-                    開始時間：<select class="stime1" name="offerStarttimeh1"></select>：<select class="sminute1" name="offerStarttimem1"></select> 〜 終了時間：<select class="ftime1" name="offerFinishtimeh1"></select>：<select class="fminute1" name="offerFinishtimem1"></select>
+                <div class="timearea">
+                    希望開始時間<br>
+                    <select class="stime1" name="offerStarttimeh1"></select>：<select class="sminute1" name="offerStarttimem1"></select> 〜 <select class="ftime1" name="offerFinishtimeh1"></select>：<select class="fminute1" name="offerFinishtimem1"></select>
                 </div>
                 <p>
                     第2希望：
                     <input type="text" name="offerDate2" class="datepicker" value=<?php echo $offerDate2 ?>>
                 </p>
                 
-                <div>
-                    希望時間<br>
-                    開始時間：<select class="stime2" name="offerStarttimeh2"></select>：<select class="sminute2" name="offerStarttimem2"></select> 〜 終了時間：<select class="ftime2" name="offerFinishtimeh2"></select>：<select class="fminute2" name="offerFinishtimem2"></select>
+                <div class="timearea">
+                    希望開始時間<br>
+                    <select class="stime2" name="offerStarttimeh2"></select>：<select class="sminute2" name="offerStarttimem2"></select> 〜 <select class="ftime2" name="offerFinishtimeh2"></select>：<select class="fminute2" name="offerFinishtimem2"></select>
                 </div>
                 <p>
                     第3希望：
                     <input type="text" name="offerDate3" class="datepicker" value=<?php echo $offerDate3 ?>>
                 </p>
-                <div>
-                    希望時間<br>
-                    開始時間：<select class="stime3" name="offerStarttimeh3"></select>：<select class="sminute3" name="offerStarttimem3"></select> 〜 終了時間：<select class="ftime3" name="offerFinishtimeh3"></select>：<select class="fminute3" name="offerFinishtimem3"></select>
+                <div class="timearea">
+                    希望開始時間<br>
+                    <select class="stime3" name="offerStarttimeh3"></select>：<select class="sminute3" name="offerStarttimem3"></select> 〜 <select class="ftime3" name="offerFinishtimeh3"></select>：<select class="fminute3" name="offerFinishtimem3"></select>
                 </div>
             </form>
         </div>
-        <div>
+        <div class="btnarea">
             <a href="" class="btn">確認する</a>
         </div>
     </div>
